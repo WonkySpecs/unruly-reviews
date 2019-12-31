bcca9c5443fe924be047b535084ae3ebf522e47e
Will Taylor <w1ll100@hotmail.co.uk>
2019-12-31 00:48:25 +0000

    Basic front page content

---
+++ src/data/site/home.md
@@ -0,0 +1,5 @@
+## Welcome
+
+Welcome to FIGURE OUT A GODDAMN NAME. We're here for **to-the-point, insightful summaries and reviews of great board games**, getting right to the heart of what makes them tick. Hopefully it can help you find the perfect game for you and the people you play with, as well as rule out those which won't be to your tastes.
+
+Visit the about page for more on what you can expect to find here, or dive straight into our most recent reviews:
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,52 @@
 import React from "react"
 import Layout from "../components/layout"
+import { Link } from "gatsby"
 
-export default () => (
+export default ({data}) => {
+    let summaryNodes = data.allFile.edges.map(edge => edge.node);
+    summaryNodes.sort((s1, s2) => s1.mtime < s2.mtime);
+    return (
     <Layout>
-        <div>An introduction to the as yet unnamed site goes here.</div>
-        <div>Include a list of most recently reviewed/highlighted/random games here</div>
+        <div dangerouslySetInnerHTML={introText(data)} />
+        {summaryNodes.map(node => {
+            const summary = node.childSummariesJson;
+            return (
+                <div>
+                    <Link to={summary.fields.slug}>{summary.title}</Link>
+                    {summary.tagline != null && summary.tagline.length > 0 ? (
+                        <span> - {summary.tagline} </span>)
+                        : null}
+                </div>)
+        })}
     </Layout>
-)
+)}
+
+export const query = graphql`
+    query {
+        allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/home.md$/"}}) {
+            edges {
+                node {
+                    html
+                }
+            }
+        }
+        allFile(filter: {childSummariesJson: {title: {ne: null}}}) {
+            edges {
+                node {
+                    mtime
+                    childSummariesJson {
+                        title
+                        tagline
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
+
+function introText(data) {
+    return { __html: data.allMarkdownRemark.edges[0].node.html };
+}
